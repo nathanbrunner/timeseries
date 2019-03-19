9ab29bebd6c6d5139d610dcd355b797b77b646d3
nathanbrunner <48694395+nathanbrunner@users.noreply.github.com>
2019-03-19 20:00:16 +0100

    Add files via upload

--- CO2project.R
+++ CO2project.R
@@ -6,10 +6,10 @@ jubany <- read.table("JubanyCleaned.txt", header = TRUE, sep="\t")
 co2 <- ts(jubany[,2],frequency=12,start=c(1994,3))
 
 ## We plot the data
-plot(co2)
+plot(co2,main="CO2 in Jubany",xlab="Years", ylab="Atmospheric CO2 (ppmv)")
 
 # STL decomposition with approximation for missing values
 install.packages("zoo")
 library("zoo")
 stl<-stl(co2, na.action = na.approx, s.window=12)
-plot(stl)
+plot(stl,main="STL decomposition")
